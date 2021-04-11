55c7d1f2c5644e2f341800e458e9914773c8403f
Louis Nodskov <lnodskov@unomaha.edu>
2021-04-11 17:02:48 -0500

    Added contribution as a subclass, Added addContribution, Added constructor

--- Project/src/main/java/com/slrp/api/model/Contributor.java
+++ Project/src/main/java/com/slrp/api/model/Contributor.java
@@ -2,7 +2,10 @@ package com.slrp.api.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+
+import java.util.ArrayList;
 import java.util.Map;
+import com.slrp.api.model.Student.Loan;
 
 /**
  * Contributor Java File
@@ -12,6 +15,48 @@ import java.util.Map;
 @Entity
 public class Contributor extends User {
 	
+	private ArrayList<Contribution> contributions;
+
+	public Contributor(String Name, String contactInfo, String address, ArrayList<Contribution> contributions) {
+		super(Name, contactInfo, address);
+		// TODO Auto-generated constructor stub
+		this.setContributions(contributions);
+	}
+	
+	public static class Contribution {
+		
+		private Loan loan;
+		private double contributionAmt;
+		// TODO: This will be changed later to be using the date class.
+		// Connor and I have decided on using MM/DD/YYYY as a norm for our date for the time being. 
+		private String date;
+		
+		public Contribution(Loan l, double contributionAmt, String date) {
+			this.setLoan(l);
+			this.setContributionAmt(contributionAmt);
+			this.setDate(date);
+		}
+		
+		public Loan getLoan() {
+			return loan;
+		}
+		public void setLoan(Loan loan) {
+			this.loan = loan;
+		}
+		public double getContributionAmt() {
+			return contributionAmt;
+		}
+		public void setContributionAmt(double contributionAmt) {
+			this.contributionAmt = contributionAmt;
+		}
+		public String getDate() {
+			return date;
+		}
+		public void setDate(String date) {
+			this.date = date;
+		}
+	}
+
 	/*
 	 * name: either a business name or personal name.
 	 */
@@ -39,8 +84,9 @@ public class Contributor extends User {
 	/*
 	 * contributions: A map where each key is a user and the value is the contribution amount.
 	 */
-	@Column(name="contributions")
-	private Map<User, Double> contributions;
+//	@Column(name="contributions")
+//	private Map<User, Double> contributions;
+// Keeping this here just in case the idea of making this a class doesn't work.
 
 	public String getName() {
 		return name;
@@ -72,17 +118,17 @@ public class Contributor extends User {
 
 	public void setVerifiedContributor(boolean verifiedContributor) {
 		this.verifiedContributor = verifiedContributor;
-	}
-
-	public Map<User, Double> getContributions() {
+	}	
+	
+	public ArrayList<Contribution> getContributions() {
 		return contributions;
 	}
 
-	public void setContributions(Map<User, Double> contributions) {
+	public void setContributions(ArrayList<Contribution> contributions) {
 		this.contributions = contributions;
 	}
 	
-	
-	
-
+	public void addContribution(Contribution c) {
+		this.getContributions().add(c);
+	}
 }
