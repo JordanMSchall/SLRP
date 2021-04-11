7167be8d9e2460544782e60e62d820f3e6df1568
Louis Nodskov <lnodskov@unomaha.edu>
2021-04-11 18:35:52 -0500

    Thymeleaf fixed

--- Project/src/main/java/com/slrp/controller/HubController.java
+++ Project/src/main/java/com/slrp/controller/HubController.java
@@ -1,6 +1,6 @@
 package com.slrp.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.slrp.util.DatabaseUtil;
 
 import java.util.Map;
 
@@ -9,12 +9,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HubController {
-	
+
 	@RequestMapping("/")
 	public String index(Map<String, Object> model) {
 		// Jordans controller
 		// map java entity
-		model.put("message", "HowToDoInJava Reader !!");
+		String [] messages = {"Hello", "World"};
+		model.put("messages", messages);
+		return "index";
+	}
+
+	@RequestMapping("/index")
+	public String index2(Map<String, Object> model) {
+		// Jordans controller
+		// map java entity
+		String [] messages = {"Hello", "World"};
+		model.put("messages", messages);
 		return "index";
 	}
 	
@@ -34,5 +44,7 @@ public class HubController {
 		// Jordans controller
 		return "test";
 	}
+
+
 
 }
